7534ad208a0aeeef617e0a901c41e8d18df4de80
SheillaO <sheilla.alusa@strathmore.edu>
2025-06-29 01:01:20 +0300

    Search Functionality API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "7601b0fff0179o9d5059a8db34ctbc66";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city")
-cityElement.innerHTML= searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-
-
-
-
-let searchFormElement = document.querySelector("#search-form")
-console.log(searchFormElement);
-searchFormElement.addEventListener("submit", handleSearchSubmit)
+searchCity("Nairobi");
